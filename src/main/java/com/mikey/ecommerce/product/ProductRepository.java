6e1eb91d91a3a8240691f54063b63066bfb28b95
Mikey <mikeywestman@gmail.com>
2026-04-26 20:57:06 +0200

    feat: add pagination sorting and filtering closes #2

--- src/main/java/com/mikey/ecommerce/product/ProductRepository.java
+++ src/main/java/com/mikey/ecommerce/product/ProductRepository.java
@@ -1,6 +1,17 @@
 package com.mikey.ecommerce.product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByNameContainingIgnoreCaseAndPriceBetween(
+            String name,
+            BigDecimal minPrice,
+            BigDecimal maxPrice,
+            Pageable pageable
+    );
 }
