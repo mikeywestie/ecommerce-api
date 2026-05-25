d92ba21f940e259fa759931f7a4d38e3c00e2b53
Mikey <mikeywestman@gmail.com>
2026-05-25 18:29:13 +0200

    Fix CI test container configuration

--- src/test/java/com/mikey/ecommerce/EcommerceApiTestcontainersTest.java
+++ src/test/java/com/mikey/ecommerce/EcommerceApiTestcontainersTest.java
@@ -5,9 +5,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @Testcontainers
@@ -18,28 +19,16 @@ class EcommerceApiTestcontainersTest {
             new PostgreSQLContainer<>("postgres:16");
 
     @Container
-    static GenericContainer<?> kafka =
-            new GenericContainer<>("confluentinc/cp-kafka:7.6.1")
-                    .withExposedPorts(9092);
+    static KafkaContainer kafka =
+            new KafkaContainer(DockerImageName.parse("apache/kafka-native:3.8.0"));
 
     @DynamicPropertySource
-    static void configureProperties(
-            DynamicPropertyRegistry registry
-    ) {
-        registry.add(
-                "spring.datasource.url",
-                postgres::getJdbcUrl
-        );
-
-        registry.add(
-                "spring.datasource.username",
-                postgres::getUsername
-        );
-
-        registry.add(
-                "spring.datasource.password",
-                postgres::getPassword
-        );
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+        registry.add("jwt.secret", () -> "ci-test-jwt-secret-key-that-is-long-enough-for-hmac-signing-123456789");
     }
 
     @Test
